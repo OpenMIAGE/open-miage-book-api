bf64ade7ccbc49865e79a1d4a3675301cd1a4bd4
gsaunier <gael.saunier@open-miage.fr>
2013-04-01 14:41:17 +0200

    add user is community moderator information

--- src/OpenM-Book/api/OpenM_Book_Const.interface.php
+++ src/OpenM-Book/api/OpenM_Book_Const.interface.php
@@ -21,26 +21,27 @@ Import::php("OpenM-Services.api.OpenM_Service");
  * @author Gaël Saunier
  */
 interface OpenM_Book_Const extends OpenM_Service {
-    
+
     const RETURN_ERROR_MESSAGE_USER_NOT_REGISTERED_VALUE = "you're not registered";
     const RETURN_ERROR_MESSAGE_USER_NOT_ACTIVATED_VALUE = "you're not activated";
-    const RETURN_ERROR_MESSAGE_USER_NOT_FOUND_VALUE = "user not found";    
+    const RETURN_ERROR_MESSAGE_USER_NOT_FOUND_VALUE = "user not found";
     const RETURN_ERROR_MESSAGE_GROUP_NOT_FOUND_VALUE = "group not found";
-    
+
     //return users parameters
     const RETURN_USER_LIST_PARAMETER = "USER_LIST";
     const RETURN_USER_ID_PARAMETER = "ID";
-    
+
     //parameters patterns
     const USER_ID_PARAMETER_PATERN = "/^[1-9][0-9]*$/";
     const GROUP_ID_PARAMETER_PATERN = "/^[1-9][0-9]*$/";
     const ID_PARAMETER_PATERN = "/^[1-9][0-9]*$/";
-    
+
     //return community parameters
     const RETURN_COMMUNITY_NAME_PARAMETER = "CNA";
     const RETURN_COMMUNITY_ID_PARAMETER = "CID";
     const RETURN_COMMUNITY_PARENT_PARAMETER = "CPP";
     const RETURN_COMMUNITY_CHILDS_PARAMETER = "CCP";
+    const RETURN_YOU_ARE_COMMUNITY_MODERATOR_PARAMETER = "YACM";
 
 }
 
